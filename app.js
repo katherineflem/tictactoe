2b4d954a58a549faed3dee8d60ad550abb3582b4
katherineflem <katherinemarief@gmail.com>
2019-02-27 22:48:42 -0600

    issue with getting the winner

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let currentPlayer = "X"
+let gameOver = false
 
 var cells = document.querySelectorAll(".row > div");// cells is the array of your rows/divs
 for (let i = 0; i < cells.length; i++) {
@@ -8,21 +9,43 @@ for (let i = 0; i < cells.length; i++) {
     }
 }
 
-function cellClicked(event){
-    event.target.textContent = "X";
+function cellClicked(event) {
+    if (gameOver === true) {
+        return;
+    }
+    if (event.target.textContent === "X" || event.target.textContent === "O") {
+        return;
+    }
+    event.target.textContent = currentPlayer;
+    checkWin();
     togglePlayer();
+
+}
+
+//toggle players
+function togglePlayer() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O"
+    } else {
+        currentPlayer = "X"
+    }
 }
 
 
+//check for a winner
+function checkWin() {
+    if (checkCombo(0, 1, 2) || checkCombo(6, 7, 8) || checkCombo(0, 3, 6) || checkCombo(1, 4, 7) ||
+        checkCombo(2, 5, 8) || checkCombo(0, 4, 8) || checkCombo(2, 4, 6)) {
+        gameOver = true;
+        console.log(currentPlayer + " is the winner!");
 
+    }
+}
 
-//toggle players
-
-function togglePlayer() {
-    event.target.textContent = currentPlayer;
-if (currentPlayer === "X"){
-    currentPlayer = "O"
-} else {
-    currentPlayer = "X"
-}}
+//check each possible combo of cells for a win
+function checkCombo(a, b, c) {
+    if (cells[a].textContent === cells[b].textContent || cells[b].textContent === cells[c].textContent && cells[a].textContent !== "") {
+        return true
+    }
+}
 
