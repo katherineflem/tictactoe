5f77b4fc026223f41bbb4f47ccf6879d6f649588
katherineflem <katherinemarief@gmail.com>
2019-03-04 19:25:21 -0600

    trying to reset board

--- app.js
+++ app.js
@@ -32,20 +32,36 @@ function togglePlayer() {
 }
 
 
-//check for a winner
+//check for a winner-- if all of these combos are have the same content, then gameover = true meaning the cellClicked function will end
 function checkWin() {
     if (checkCombo(0, 1, 2) || checkCombo(6, 7, 8) || checkCombo(0, 3, 6) || checkCombo(1, 4, 7) ||
         checkCombo(2, 5, 8) || checkCombo(0, 4, 8) || checkCombo(2, 4, 6)) {
+        alert(currentPlayer + " is the winner!")
         gameOver = true;
-        console.log(currentPlayer + " is the winner!");
+    } else {
+        alert("It's a Draw!") // if there is no winner and the board is full then It is a draw
 
     }
 }
 
 //check each possible combo of cells for a win
 function checkCombo(a, b, c) {
-    if (cells[a].textContent === cells[b].textContent || cells[b].textContent === cells[c].textContent && cells[a].textContent !== "") {
+    if ((cells[a].textContent === cells[b].textContent || cells[b].textContent === cells[c].textContent) && (cells[a].textContent !== "" && cells[b].textContent !== "" && cells[c].textContent !== "")) {
         return true
     }
-}
 
+
+    //reset board-- want to make it so this is the result of a winning game or a draw -- make the board clear all text content 
+    let btn = document.queryselector("button");
+    btn.addEventListener("click", resetBoard)
+
+
+    function resetBoard() {
+        for (let i = 0; i < cells.length; i++) {
+            console.log(cells[i]); {
+                cells[i] = ("")
+                return;
+            }
+        }
+    }
+} 
